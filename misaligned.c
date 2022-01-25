1afef56dc2b7e17b29552e956200316fd6a2bdc7
SathishKumar Loganathan <43195344+SathishKumar-Loganathan@users.noreply.github.com>
2022-01-25 23:23:59 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -45,7 +45,7 @@ void unitTestColorMapforRandomPairNumber(int expectedNumberPair) {
 
 int main() {
     int result = printColorMap();
-    assert(result == 24);
+    assert(result == 30);
     unitTestColorMapforRandomPairNumber(5);
     unitTestColorMapforRandomPairNumber(23);
     unitTestColorMapforRandomPairNumber(-4);
