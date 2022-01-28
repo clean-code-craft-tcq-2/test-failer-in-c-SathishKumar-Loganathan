7f4deafcba5fee553da425dd6c6b2680656db247
SathishKumar Loganathan <43195344+SathishKumar-Loganathan@users.noreply.github.com>
2022-01-28 21:32:37 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -16,28 +16,26 @@ float getCelsiusFromFarenheit(float farenheit)  {
 	return (farenheit - 32) * 5 / 9;
 }
 
-void alertInCelcius(float farenheit, int (*pointsToNetworkAlertStub)(float))
+void alertInCelcius(float farenheit, float (*getCelsiusFromFarenheit(float), int (*pointsToNetworkAlertStub)(float))
 {
     float celcius = getCelsiusFromFarenheit(farenheit);
     int returnCode = pointsToNetworkAlertStub(celcius);
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
-        // However, this code doesn't count failures!
-        // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+        alertFailureCount += 1;
     }
 }
 
 int main() {
     // Test above threshold
 	assert(getCelsiusFromFarenheit(400.5) == 204.722222f);
-    alertInCelcius(400.5, &networkAlertStub);
+    alertInCelcius(400.5, &getCelsiusFromFarenheit, &networkAlertStub);
     assert(alertFailureCount == 1);
     
     //Test below threshold
 	assert(getCelsiusFromFarenheit(303.6) == 150.888889f);
-    alertInCelcius(303.6, &networkAlertStub);
+    alertInCelcius(303.6, &getCelsiusFromFarenheit, &networkAlertStub);
     assert(alertFailureCount == 1);
     
     printf("%d alerts failed.\n", alertFailureCount);
