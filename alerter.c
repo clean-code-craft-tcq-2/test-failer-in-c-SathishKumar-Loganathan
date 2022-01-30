fefa9fa89c254c27514faf4b634427bf7b4fdbc2
SathishKumar Loganathan <43195344+SathishKumar-Loganathan@users.noreply.github.com>
2022-01-30 19:13:05 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -4,41 +4,36 @@
 int alertFailureCount = 0;
 
 int networkAlertStub(float celcius) {
-    if(celcius > 200.0f) {
+    
+    if(celcius > 200.0)
+        return 200;     // Return 200 for ok
+    else
+    {
         printf("ALERT: Temperature is %.1f celcius.\n", celcius);
         return 500;     // Return 500 for not-ok
     }
-    else
-        return 200;    // Return 200 for ok
+    
+    // stub always succeeds and returns 200
+    // return 200;
 }
 
-float getCelsiusFromFarenheit(float farenheit)  {
-	return (farenheit - 32) * 5 / 9;
-}
-
-void alertInCelcius(float farenheit, float (*getCelsiusFromFarenheit)(float), int (*pointsToNetworkAlertStub)(float))
-{
-    float celcius = getCelsiusFromFarenheit(farenheit);
-    int returnCode = pointsToNetworkAlertStub(celcius);
+void alertInCelcius(float farenheit) {
+    float celcius = (farenheit - 32) * 5 / 9;
+    int returnCode = networkAlertStub(celcius);
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
-        alertFailureCount += 1;
+        // However, this code doesn't count failures!
+        // Add a test below to catch this bug. Alter the stub above, if needed.
+        alertFailureCount += 0;
     }
 }
 
 int main() {
-    // Test above threshold
-	assert(getCelsiusFromFarenheit(400.5) == 204.722222f);
-    alertInCelcius(400.5, &getCelsiusFromFarenheit, &networkAlertStub);
+    alertInCelcius(400.5);
     assert(alertFailureCount == 1);
-    
-    //Test below threshold
-	assert(getCelsiusFromFarenheit(303.6) == 150.888889f);
-    alertInCelcius(303.6, &getCelsiusFromFarenheit, &networkAlertStub);
-    assert(alertFailureCount == 1);
-    
+    alertInCelcius(303.6);
     printf("%d alerts failed.\n", alertFailureCount);
-    printf("All is well \n");
+    printf("All is well (maybe!)\n");
     return 0;
 }
